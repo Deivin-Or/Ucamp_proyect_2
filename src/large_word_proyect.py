16683d4e145511464af783d05d872f2f57bebc12
Deivin_Bal <baldeivin82@gmail.com>
2024-09-08 22:15:29 -0600

    "Actualización de Toots"

--- src/large_word_proyect.py
+++ src/large_word_proyect.py
@@ -2,10 +2,31 @@
 # Bienvenida usuario y presentación de Toots
 print("""Bienvenido al detector de longitud de palabra
 ¡Hola soy Toots! Tu revisador de palabras, te indicaré si tu palabra tiene
-la longitud correcta para poder ser un hashtag de Twitter :v\n
+la longitud correcta para poder ser un hashtag de Twitter **o X en este caso** :v\n
+
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⠾⠛⠋⠉⠉⠀⠀⠀⠉⠉⠉⠛⠻⠶⣤⣄⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣠⡶⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠷⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⡾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢶⣤⡀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣴⣯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣿⣶⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠰⣼⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠈⠻⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⡐⣴⠟⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣾⣷⡄⠀⠀⠀⠀⠀⠀
+⣬⣿⠃⠀⠀⢠⣤⡄⠀⠀⠀⠀⠉⠲⠀⠀⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⢷⣤⣄⣀⣀⡀⠀
+⣿⣿⣄⠀⠀⣸⣿⡁⠀⠀⠀⠀⠀⣴⠀⠀⠈⠛⠻⠿⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢷⣤⡽⠇⠀
+⣿⣿⡛⠛⠛⠋⠈⠻⢦⣤⣠⣤⡾⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣶⡄
+⠈⠛⠿⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⡶⢾⠛⠇
+⠀⠀⠀⠀⢭⡛⣷⢶⣤⣄⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡀⠀⠀⠀⠀⠀⢿⣄⣀⣤⣤⣴⣶⠟⠛⠋⢉⣤⣶⣮⠀⠀
+⠀⠀⠀⠀⠀⣶⣇⠀⢀⣽⠟⠛⠛⠛⠛⠿⠿⠷⠶⠶⠶⠶⠶⠶⠶⠶⠾⢷⣤⣀⠀⢀⣠⣾⠟⠉⠁⠀⠈⢿⡟⠶⣶⣿⣿⡿⠟⠀⠀
+⠀⠀⠀⠀⠀⠉⠛⢷⣾⠇⠀⢀⣤⣤⡀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠘⣷⠀⠸⣏⠹⣧⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣼⡏⠀⠀⢺⣿⣿⡇⠀⠀⣟⠀⢰⣆⠀⠘⣷⠀⠀⠀⢰⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠻⣦⡿⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢸⣿⡇⢰⢤⡌⠉⠁⠀⠀⠀⠻⠶⠟⠙⠷⠾⠋⠀⠀⠀⠘⠿⠿⠿⠁⣀⢠⡄⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠈⣿⣷⠈⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⠚⠋⠀⠀⠀⠀⢠⡟⡆⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠁⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣄⠀⢿⣟⢷⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡧⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢀⣤⢀⣆⣤⣀⣿⠀⢸⡏⠙⠛⠿⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠘⣛⣿⡿⣿⡿⢛⡁⠸⢷⣀⠀⠀⠀⠈⠙⡛⠷⢶⣤⣤⣤⣀⣀⣀⣀⣀⣀⣠⣤⣤⡶⠾⣿⣋⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      
     ¡Empecemos!\n""")
-
-
 
 while True:
     word = input("Escribe tu palabra, Toots te espera: ")
@@ -15,10 +36,10 @@ while True:
         break
     elif word_longer < 4:
         missing = 4 - word_longer
-        print(f"Toots detecta que tu palabra {word} es demasiado corta\n Letras que le faltan a Toots --->{ missing}\nToots reinicia proceso.")
+        print(f"Toots detecta que tu palabra {word} es demasiado corta\n Letras que le faltan a Toots --->{ missing}\nToots reinicia proceso.\n")
     elif word_longer > 8:
         missing = word_longer - 8
-        print(f"Toots detecta que tu palabra {word} es demasiado larga\n Letras que le sobran a Toots ---> {missing}")
+        print(f"Toots detecta que tu palabra {word} es demasiado larga\n Letras que le sobran a Toots ---> {missing}\nToots reinicia proceso.\n")
 
 print(f"""-----------------------------------------------------------------------------
 La palabra {word} es aprobada por Toots tiene la longitud correcta.\n
