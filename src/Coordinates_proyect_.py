d17b8bf04da335b7be240242eda05ac7b746b17d
Deivin_Bal <baldeivin82@gmail.com>
2024-09-08 22:08:14 -0600

    "Subida de actualización de archivos y large_proyect"

--- src/Coordinates_proyect_.py
+++ src/Coordinates_proyect_.py
@@ -44,6 +44,14 @@ for position_dot in range(num_dot):
             print("Toto no acepta valores nulos")
         elif x_axis .isalpha():
             print("Toto necesita un valor númerico")
+        elif not (x_axis.isdigit() or (x_axis.startswith('-') and x_axis[1:].isdigit())):
+            # x_axis.isdigit() evalua si es un numero y si encuntra al simbolo diferente a ello que marque true pero lo invierto con not
+            # para poder continuar ya que quiero que de true a la hora que ideintifique alguna letra
+            # (x_axis.startswith('-') and x_axis[1:].isdigit()) es por que quiero que a la hora de ingresar negativos los reconozco como numeros
+            # ya que isdigit no reconoce el - tengo que hacer una función que de true a la hora de encontarlo para poder invertirlo al final con not
+            # y el programa continue
+
+            print("Toto no puede trabajar vales mixtos")
         else:
             x_axis = int(x_axis)
             break
@@ -51,24 +59,26 @@ for position_dot in range(num_dot):
 
     # Ingreso de coordenadas en y
     while True:
-        y_axis_in = input("Toto necesita la coordenada en el eje y: ")
-        if y_axis_in == (""):
+        y_axis = input("Toto necesita la coordenada en el eje y: ")
+        if y_axis == (""):
             print("Toto no puede trabajar con un valor vacío")
-        elif y_axis_in == ("0"):
+        elif y_axis == ("0"):
             print("Toto no acepta valores nulos")
-        elif y_axis_in.isalpha():
+        elif y_axis.isalpha():
             print("Toto necesita un valor númerico")
+        elif not (y_axis.isdigit() or (y_axis.startswith('-') and y_axis[1:].isdigit())):
+            print("Toto no puede trabajar vales mixtos")
         else:
-            y_axis = int(y_axis_in)
+            y_axis = int(y_axis)
             break
     y_coords.append(y_axis)
 
     # Lista de respuestas
     message_quadrant = [
-    """Toto detecta \nel cuadrante I.\n""",
-    """Toto detecta \nel cuadrante II.\n""",
-    """Toto detecta \nel cuadrante III.\n""",
-    """Toto detecta \nel cuadrante IV.\n"""
+    """Toto detecta el cuadrante I.""",
+    """Toto detecta el cuadrante II.""",
+    """Toto detecta el cuadrante III.""",
+    """Toto detecta el cuadrante IV."""
     ]
 
     if x_axis > 0 and y_axis > 0:
@@ -81,11 +91,11 @@ for position_dot in range(num_dot):
         result.append(message_quadrant[2])
         dot_color.append("purple")
     elif x_axis > 0 and y_axis < 0:
-        result = message_quadrant[3]
+        result.append(message_quadrant[3])
         dot_color.append("blue")
 
 
-# # Creación del gráfico
+# Creación y configuración del gráfico que generamos
 fig, ax = graf.subplots()
 ax.axhline(0, color = "black", linewidth=1)  # Eje X
 ax.axvline(0, color = "black", linewidth=1)  # Eje Y
@@ -94,14 +104,18 @@ ax.set_ylim(-50, 50)
 ax.grid(True, which = "both", linestyle = "--", linewidth = 0.5)
 
 # Marcación de los puntos y anotación con coordenadas
-for x_axis, y_axis, result, color in zip(x_coords, y_coords, result, dot_color):
+for x_axis, y_axis, quadrant_result, color in zip(x_coords, y_coords, result, dot_color):
     if x_axis != 0 or y_axis != 0:
         ax.plot(x_axis, y_axis, "o", color=color)
         message = f"{result}\nCoordenadas: ({x_axis}, {y_axis})"
         ax.text(x_axis, y_axis, message, fontsize=12, color=color, ha="right")
 
-# Mostrar el gráfico
+# Función que muestra el gráfico final
 graf.show()
 
+
+print("Las coordenadas ingresadas y los cuadrantes correspondientes son:")
+for result_list in range(num_dot):
+    print(f"Punto {result_list + 1}: Coordenadas ({x_coords[result_list]}, {y_coords[result_list]}) - {result[result_list]}")
 
 print("Toto ha terminado su trabajo, muchas gracias por usarlo.")
---
+++ src/large_word_proyect.py
@@ -0,0 +1,26 @@
+# Inicio proyecto longitud de palabra
+# Bienvenida usuario y presentación de Toots
+print("""Bienvenido al detector de longitud de palabra
+¡Hola soy Toots! Tu revisador de palabras, te indicaré si tu palabra tiene
+la longitud correcta para poder ser un hashtag de Twitter :v\n
+    ¡Empecemos!\n""")
+
+
+
+while True:
+    word = input("Escribe tu palabra, Toots te espera: ")
+    word_longer = len(word)
+    if 4 <= word_longer <= 8:
+        print("La cantidad de letras es correcta puede seguir, Toots te felicita")
+        break
+    elif word_longer < 4:
+        missing = 4 - word_longer
+        print(f"Toots detecta que tu palabra {word} es demasiado corta\n Letras que le faltan a Toots --->{ missing}\nToots reinicia proceso.")
+    elif word_longer > 8:
+        missing = word_longer - 8
+        print(f"Toots detecta que tu palabra {word} es demasiado larga\n Letras que le sobran a Toots ---> {missing}")
+
+print(f"""-----------------------------------------------------------------------------
+La palabra {word} es aprobada por Toots tiene la longitud correcta.\n
+¡Felicidades Toots te desea un buen día, hasta la próxima :3""")
+
