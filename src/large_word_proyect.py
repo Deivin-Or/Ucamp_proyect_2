120170a137d7853a93ab46b3784eccd7e3142455
Deivin_Bal <baldeivin82@gmail.com>
2024-09-08 22:24:42 -0600

    "Actualización extra"

--- src/large_word_proyect.py
+++ src/large_word_proyect.py
@@ -2,7 +2,7 @@
 # Bienvenida usuario y presentación de Toots
 print("""Bienvenido al detector de longitud de palabra
 ¡Hola soy Toots! Tu revisador de palabras, te indicaré si tu palabra tiene
-la longitud correcta para poder ser un hashtag de Twitter **o X en este caso** :v\n
+la longitud correcta para poder ser un hashtag de Twitter **o X en este caso** :3\n
 
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣤⣤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⠾⠛⠋⠉⠉⠀⠀⠀⠉⠉⠉⠛⠻⠶⣤⣄⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -32,16 +32,17 @@ while True:
     word = input("Escribe tu palabra, Toots te espera: ")
     word_longer = len(word)
     if 4 <= word_longer <= 8:
-        print("La cantidad de letras es correcta puede seguir, Toots te felicita")
+        print("\nLa cantidad de letras es correcta puede seguir, Toots te felicita\n")
         break
     elif word_longer < 4:
         missing = 4 - word_longer
-        print(f"Toots detecta que tu palabra {word} es demasiado corta\n Letras que le faltan a Toots --->{ missing}\nToots reinicia proceso.\n")
+        print(f"Toots detecta que tu palabra {word} es demasiado corta\n Letras que le faltan a Toots ---> { missing}\nToots reinicia proceso.\n")
     elif word_longer > 8:
         missing = word_longer - 8
         print(f"Toots detecta que tu palabra {word} es demasiado larga\n Letras que le sobran a Toots ---> {missing}\nToots reinicia proceso.\n")
 
 print(f"""-----------------------------------------------------------------------------
 La palabra {word} es aprobada por Toots tiene la longitud correcta.\n
-¡Felicidades Toots te desea un buen día, hasta la próxima :3""")
+¡Felicidades Toots te desea un buen día, hasta la próxima :3
+-----------------------------------------------------------------------------""")
 
